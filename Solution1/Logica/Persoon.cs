34a738f9938f9babd0cf14467086b4c4dab4d8bc
numbertwoplz <numbertwoplz@gmail.com>
2020-09-10 19:43:45 +0200

    useless commit

---
+++ Solution1/Logica/Persoon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public class Persoon
+    {
+        String Naam;
+        public Persoon(String naam)
+        {
+            Naam = naam;
+        }
+
+        public override string ToString()
+        {
+            return "Mijn naam is " + this.Naam ;
+        }
+    }
+}
